25f95668431a7157a71673c0f3fe117cceecc6b7
uan.nguyen <uan.nguyen@finviet.com.vn>
2024-03-08 16:58:50 +0700

    update init commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  await app.listen(+process.env.PORT, () => {
+    Logger.log(
+      'Listening at http://localhost:' + +process.env.PORT + '/' + globalPrefix,
+    );
+  });
 }
 bootstrap();
